3482b6b12687cda039b426a2248c685edff5e54e
Sudheer Akula <sudakula@teksystems.com>
2021-05-27 00:57:45 -0500

    initial commit

---
+++ examples/basic/variables.tf
@@ -0,0 +1,8 @@
+variable "region"{
+    type = string
+}
+
+
+variable "sg_group"{
+    type = string
+}
--- examples/basic/versions.tf
+++ examples/basic/versions.tf
@@ -4,4 +4,12 @@ terraform {
   required_providers {
     aws = ">= 2.65"
   }
+
+  backend "s3" {
+    bucket  = "terraform-state1432"
+    key     = "terraform.tfstate"
+    region  = "us-west-2"
+    encrypt = true
+  }
+
 }
